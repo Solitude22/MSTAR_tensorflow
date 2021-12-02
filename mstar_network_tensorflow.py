894a00948fec14a9027511b358edf3670397ac88
akittel3 <akittel3@gatech.edu>
2021-12-02 18:31:20 -0500

    cleaned up mstar_network_tensorflow, added comments

--- mstar_network_tensorflow.py
+++ mstar_network_tensorflow.py
@@ -1,48 +1,28 @@
 """
-Copy of mstart_network.py using minimal effort to train a Tensorflow model over tflearn.
+Trimmed version of mstar_network.py from the original repo.
+
+mstarnet has been replaced with a deeper CNN with 5 convolution layers. This model has been used 
+in the past on other projects and has proven to be a high performing model without high complexity.
+
+Alan Kittel
+The Aerospace Corporation
+12/2/21
 """
+
 import tensorflow as tf
 import tflearn
 import numpy as np
 from data import DataHandler
-#from network_defs import *
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import layers, models
 from tensorflow.keras.models import load_model
 from matplotlib import pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 
-def example_net(x, classes):
-	network = tflearn.conv_2d(x, 32, 3, activation='relu')
-	network = tflearn.max_pool_2d(network, 2)
-	network = tflearn.conv_2d(network, 64, 3, activation='relu')
-	network = tflearn.conv_2d(network, 64, 3, activation='relu')
-	network = tflearn.max_pool_2d(network, 2)
-	network = tflearn.fully_connected(network, 512, activation='relu')
-	network = tflearn.dropout(network, 0.5)
-	network = tflearn.fully_connected(network, classes, activation='softmax')
-
-	return network
-
-
-def trythisnet(x, classes):
-	network = tflearn.conv_2d(x,64,5,activation='relu')
-	network = tflearn.max_pool_2d(network,3,2)
-	network = tflearn.local_response_normalization(network,4,alpha=0.001/9.0)
-	network = tflearn.conv_2d(network,64,5,activation='relu')
-	network = tflearn.local_response_normalization(network,4,alpha=0.001/9.0)
-	network = tflearn.max_pool_2d(network,3,2)
-	network = tflearn.fully_connected(network,384,activation='relu',weight_decay=0.004)
-	network = tflearn.fully_connected(network,192,activation='relu',weight_decay=0.004)
-	network = tflearn.fully_connected(network,classes,activation='softmax',weight_decay=0.0)
-
-	return network
-
 """
 Implementation of mstartnet using Tensorflow.
 
 mstarnet is a basic conv-net in mstar_network.py. Here a similarly structured but deeper network is used.
-
 """
 def mstarnet(x, classes):
 	model = models.Sequential()
@@ -64,27 +44,8 @@ def mstarnet(x, classes):
 	
 	return model
 
-def resnet1(x, classes, n = 5):
-	net = tflearn.conv_2d(x, 16, 3, regularizer='L2', weight_decay=0.0001)
-	net = tflearn.residual_block(net, n, 16)
-	net = tflearn.residual_block(net, 1, 32, downsample=True)
-	net = tflearn.residual_block(net, n - 1, 32)
-	net = tflearn.residual_block(net, 1, 64, downsample=True)
-	net = tflearn.residual_block(net, n - 1, 64)
-	net = tflearn.batch_normalization(net)
-	net = tflearn.activation(net, 'relu')
-	net = tflearn.global_avg_pool(net)
-	# Regression
-	net = tflearn.fully_connected(net, classes, activation='softmax')
-
-	return net
-
 def train_nn_tflearn(data_handler,modelSave,targets,num_epochs=50):
 
-	#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
-	#tflearn.init_graph(gpu_memory_fraction=0.5)
-
-	batch_size = data_handler.mini_batch_size
 	classes = data_handler.num_labels
 
 	img_prep = tflearn.ImagePreprocessing()
@@ -94,53 +55,21 @@ def train_nn_tflearn(data_handler,modelSave,targets,num_epochs=50):
 	img_aug = tflearn.ImageAugmentation()
 	img_aug.add_random_flip_leftright()
 	img_aug.add_random_rotation(max_angle=25)
-	#img_aug.add_random_crop([32,32], padding=4)
 
 	x = tflearn.input_data(shape=[None, 128, 128, 1], dtype='float', data_preprocessing=img_prep,
 						   data_augmentation=img_aug)
-	# x = tf.placeholder('float', [None, 32, 32, 3])
-	#y = tf.placeholder('float', [None, 10])
-
-	# test_data, test_labels = data_handler.get_test_data()
-	# test_data = test_data.reshape([-1,32,32,3])
-
-	ntrain = data_handler.train_size
-	ntest = data_handler.meta['num_cases_per_batch']
-
-	# from tflearn.datasets import cifar10
-	# (X, Y), (X_test, Y_test) = cifar10.load_data(dirname="/home/hamza/meh/bk_fedora24/Documents/tflearn_example/cifar-10-batches-py")
-	# X, Y = tflearn.data_utils.shuffle(X, Y)
-	# Y = tflearn.data_utils.to_categorical(Y, 10)
-	# Y_test = tflearn.data_utils.to_categorical(Y_test, 10)
 
 	X, Y = data_handler.get_all_train_data()
 
 	X, Y = tflearn.data_utils.shuffle(X, Y)
-
-	#X = np.dstack((X[:, :128*128], X[:, 128*128:]))
 	X = X[:,:128*128]
-
-	#X = X/255.0
-
-	#X = X.reshape([-1,128,128,2])
 	X = X.reshape([-1,128,128,1])
 	
 	Y = tflearn.data_utils.to_categorical(Y,classes)
 
 	X_test, Y_test = data_handler.get_test_data()
-
-	#X_test = np.dstack((X_test[:, :128*128], X_test[:, 128*128:]))
 	X_test = X_test[:,:128*128]
-	#X_test = X_test/255.0
-
-	#X_test = X_test.reshape([-1,128,128,2])
 	X_test = X_test.reshape([-1,128,128,1])
-	#network = tflearn.regression(net3(x),optimizer='adam',loss='categorical_crossentropy',learning_rate=0.001)
-	#mom = tflearn.Momentum(0.1, lr_decay=0.1, decay_step=32000, staircase=True)
-	#network = tflearn.regression(resnet1(x),optimizer='sgd',loss='categorical_crossentropy')
-	#network = tflearn.regression(resnet1(x,classes),optimizer='adam',loss='categorical_crossentropy')
-	# network = tflearn.regression(mstarnet(x,classes),optimizer='adam',loss='categorical_crossentropy')
-	#network = tflearn.regression(trythisnet(x,classes),optimizer='adam',loss='categorical_crossentropy')
 
 	"""
 	Tensorflow expects a different labeling format
@@ -186,6 +115,7 @@ def train_nn_tflearn(data_handler,modelSave,targets,num_epochs=50):
 	plt.ylabel('Accuracy')
 	plt.xlabel('Epoch')
 	plt.legend(['Train', 'Validation'], loc='lower right')
+
 	# summarize history for loss
 	plt.figure()
 	plt.plot(history.history['loss'])
@@ -209,16 +139,39 @@ def train_nn_tflearn(data_handler,modelSave,targets,num_epochs=50):
 
 	ax.xaxis.set_ticklabels(targets)
 	ax.yaxis.set_ticklabels(targets)
+
+	# show all the plots
 	plt.show()
 
+"""
+Make modifcations here.
+"""
 if __name__ == '__main__':
-	bl = "D:\My Documents\Work\Aerospace\MSTAR_tensorflow\output"
-	nb = 1
-	mbs = 32
-	nep = 20
-	modelSave = "models/mstar_targets/mstarnet/mstar_targets_model.h5"
-	# targets = ['2S1', 'BRDM2', 'BTR60', 'D7', 'T62', 'ZIL131', 'ZSU23/4']
-	targets = ['BMP2', 'BTR70', 'T72']
+	""" Change to point to the output folder location produced by readmstar.py """
+	# folder location of data produced by readmstar.py
+	data_folder = "D:\My Documents\Work\Aerospace\MSTAR_tensorflow\output"
 
-	handler = DataHandler(bl,nb,mbs)
-	train_nn_tflearn(handler,modelSave,targets,nep)
+	# number of batch files in output/ folder. Do not need to change from 1.
+	num_batch_files = 1
+
+	# batch size for model training
+	mini_batch_size = 32
+
+	# number of epochs for model training
+	epochs = 20
+
+	""" Change to desired save location """
+	# file to save trained model to
+	modelSave = "models/mstar_targets/mstarnet/mstar_targets_model.h5"
+
+	""" Change for mixed targets vs public targets dataset """
+	# string labels for the targets for creating the confusion matrix.
+	# Needs to match the order used by readmstar (alphabetical), or the confusion matrix will mislabel rows/columns.
+	# targets = ['2S1', 'BRDM2', 'BTR60', 'D7', 'T62', 'ZIL131', 'ZSU23/4'] # for mixed targets dataset
+	targets = ['BMP2', 'BTR70', 'T72'] # for public targets dataset
+
+	# call DataHandler to read the data
+	handler = DataHandler(data_folder,num_batch_files,mini_batch_size)
+
+	# train the model
+	train_nn_tflearn(handler, modelSave, targets, epochs)
